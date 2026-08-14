132fef759db8e265459109cfe02042de618d77ac
Anna Nikitina <nikitinaanna47@outlook.com>
2026-08-14 12:42:32 +0200

    fix review comments

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -16,9 +16,13 @@ export const PostInfo: React.FC<Props> = ({ post }) => (
     <div className="PostInfo__header">
       <h3 className="PostInfo__title">{post.title}</h3>
 
-      <p> Posted by {post.user && <UserInfo user={post.user} />} </p>
+      {post.user && (
+        <p>
+          {`Posted by `}
+          <UserInfo user={post.user} />
+        </p>
+      )}
     </div>
-
     <p className="PostInfo__body">{post.body}</p>
 
     <hr />
